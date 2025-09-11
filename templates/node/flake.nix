514f52506e982efc026eb6d13407cdb9fd8fd440
Thanapon Johdee <66236295+yokeTH@users.noreply.github.com>
2025-09-11 19:34:24 +0700

    feat: nodejs

---
+++ templates/node/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              nodejs
+              pnpm
+              npm-check-updates
+            ];
+          };
+      }
+    );
+}
